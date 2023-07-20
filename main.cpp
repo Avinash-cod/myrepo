5a5aced0df755805051da8273a3106ffe4b1ae3d
Avinash-cod <avinashsha76@gmail.com>
2023-07-20 10:05:38 +0530

    couting sort added

--- main.cpp
+++ main.cpp
@@ -4,14 +4,19 @@ void bubblesort(){
     //
 }
 
-
 void quicksort(){
-    
+    //
 }
 
 void mergesort(){
     //
 }
+
+void insertionsort(){
+    //
+}
+
+
 int main(){
     std::cout<<"Hello world";
     std::cout<<"Hello, Avinash Sharma";
